ae77a921e65284fc064fb889cbe17ef2ee9c1c58
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-06-08 10:16:13 -0500

    restful api

--- resourcesjs/promises.js
+++ resourcesjs/promises.js
@@ -38,15 +38,112 @@
 //         // handle errors
 //     });
 
+//
+// Promise.resolve('one').then((one) => {
+//     console.log(one);
+//     return 'two';
+// }).then((two) => {
+//     console.log(two);
+//     return 'three';
+// }).then((three) => {
+//     console.log(three);
+// });
 
-Promise.resolve('one').then((one) => {
-    console.log(one);
-    return 'two';
-}).then((two) => {
-    console.log(two);
-    return 'three';
-}).then((three) => {
-    console.log(three);
-});
+
+// fetch('https://api.github.com/users').then( response => {
+//     response.json().then( users => {
+//         users.forEach( user => {
+//             // do something with each user object...
+//             console.log(user);
+//         });
+//     });
+// });
+
+// fetch('https://api.github.com/users').then( response => {
+//     response.json().then( users => {
+//         users.forEach( userObj  => {
+//             // do something with the username login
+//             console.log(userObj.login);
+//         });
+//     });
+// });
+// fetch('https://api.github.com/users')
+//     .then(response => response.json())
+//     .then(users => {
+//         users.forEach( userObj => {
+//             // do something with each username
+//             console.log(userObj.login);
+//         })
+//     })
+//     .catch(error => console.error(error));
+//
+// function getGithubUsernames() {
+//     return fetch('https://api.github.com/users')
+//         .then(response => response.json())
+// }
+//
+// // later on...
+//
+// getGithubUsernames().then( users => {
+//     users.forEach( userObj => {
+//         // do something with each username
+//         console.log(userObj.login);
+//     });
+// }).catch(error => console.error(error));
+
+//CREATING A PROMISE//
+// const myPromise = new Promise((resolve, reject) => {
+//     if (Math.random() > 0.5) {
+//         resolve();
+//     } else {
+//         reject();
+//     }
+// });
+// console.log(myPromise)
+
+
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (Math.random() > 0.5) {
+//             resolve();
+//         } else {
+//             reject();
+//         }
+//     }, 1500);
+// });
+//
+// console.log(myPromise); // a pending promise
+//
+// myPromise.then(() => console.log('resolved!'));
+// myPromise.catch(() => console.log('rejected!'));
+
+function makeRequest() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve('Here is your data: ...');
+            } else {
+                reject('Network Connection Error!');
+            }
+        }, 1500);
+    });
+}
+
+const request = makeRequest();
+console.log(request); // pending promise
+request.then(message => console.log('Promise resolved!', message));
+// if resolved, will log "Promise resolved!" and "Here is your data: ..."
+request.catch(message => console.log('Promise rejected!', message));
+// if rejected, will log "Promise rejected!" and "Network Connection Error!"
+
+
+
+
+
+
+
+
+
+
 
 
